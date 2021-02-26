673dcc108e0fd7fdf5f0913ba68cebc94a990524
Marat <m.rafikov1986@gmail.com>
2021-02-26 23:59:48 +0600

    reverse feature

--- vice_versa/urls.py
+++ vice_versa/urls.py
@@ -19,7 +19,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
+    path('', views.home, name='homepage'),
     path('reverse/', views.reverse, name='reverse'),
 
 ]
